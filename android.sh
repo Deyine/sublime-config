8bced0a79c5355c8c01a9db5f6780155dd24806c
Deyine JIDDOU <deyine.jiddou@neticoa.fr>
2016-07-23 18:04:15 +0000

    my configs

---
+++ android.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=/home/dmahmoud/Android/Sdk
+pathmunge $ANDROID_HOME/tools after
+pathmunge $ANDROID_HOME/platform-tools after
+pathmunge $ANDROID_HOME/build-tools/23.0.3 after
---
+++ java.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr/java/jdk1.8.0_74
+pathmunge $JAVA_HOME/bin after
